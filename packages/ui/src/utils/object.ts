b338131f99d809814c2c5ec730539e731f3d2c0f
uma-uc <uma.apple123@gmail.com>
2026-07-11 10:34:36 -0700

    feat: utils objects

---
+++ packages/ui/src/utils/object.ts
@@ -0,0 +1,3 @@
+export function isJsonObject<T extends Record<string, unknown> = Record<string, unknown>>(value: unknown): value is T {
+  return value != null && typeof value === 'object' && !Array.isArray(value)
+}
